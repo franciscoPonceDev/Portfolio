7db16a9779857ed477cf00eee09935fc6f9b112e
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-01-26 18:19:07 -0300

    form is working properly now

--- form.js
+++ form.js
@@ -3,29 +3,27 @@ const form = document.getElementById("contact");
 const email = document.getElementById("email");
 
 function isEmail(email) {
-  return email.match(
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  return email.match(/^[a-z]*$/);
 }
 
 function checkInputs() {
   const emailValue = email.value.trim();
-
+  
   function setErrorFor(input, message) {
     const formControl = input.parentElement;
     const small = formControl.querySelector("small");
     formControl.className = "form-control error";
     small.innerText = message;
   }
-
-  if (emailValue === "") {
-    setErrorFor(email, "Email cannot be blank");
-  } else if (!isEmail(emailValue)) {
+  
+  if (!isEmail(emailValue)) {
     setErrorFor(email, "Not a valid email");
+  } else {
+    form.submit();
   }
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   checkInputs();
 });
