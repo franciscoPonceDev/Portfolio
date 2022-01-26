62b957618e7041337594b1a1843c4b2bedce00ee
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-01-25 21:07:17 -0300

    create projects.js

---
+++ projects.js
@@ -0,0 +1,75 @@
+const projects = [
+    {
+        title: "Multi Post Stories",
+        languages: ["html", "Bootstrap", "Ruby"],
+        imageUrl: "./assets/Img_Placeholder_1.png",
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        id: 1
+    },
+    {
+        title: "Professional Art Printing Data",
+        languages: ["html", "Bootstrap", "Ruby on rails"],
+        imageUrl: "./assets/Img_Placeholder_3.png",
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        id: 2
+    },
+    {
+        title: "Multi Post Stories",
+        languages: ["html", "Bootstrap", "Ruby"],
+        imageUrl: "./assets/Img_Placeholder_1.png",
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        id: 3
+    },
+    {
+        title: "Professional Art Printing Data",
+        languages: ["html", "Bootstrap", "Ruby on rails"],
+        imageUrl: "./assets/Img_Placeholder_3.png",
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        id: 4
+    },
+    {
+        title: "Multi Post Stories",
+        languages: ["html", "Bootstrap", "Ruby"],
+        imageUrl: "./assets/Img_Placeholder_1.png",
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        id: 5
+    },
+    {
+        title: "Professional Art Printing Data",
+        languages: ["html", "Bootstrap", "Ruby on rails"],
+        imageUrl: "./assets/Img_Placeholder_3.png",
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        id: 6
+    }
+];
+
+let cardsSection = document.getElementById("cardsSection");
+
+projects.map((project) => {
+    letHTMLElement = document.createElement("div");
+    HTMLElement.innerHTML = generateHTML(project);
+    cardsSection.appendChild(HTMLElement);
+});
+
+function generateHTML(details) {
+    let template = `
+    <div class="work-card" title="Project: Professional Art Printing Data">
+        <div class="card-content">
+            <h4>${details.title}</h4>
+            <p>${details.desc}</p>
+            <ul>
+                <li>${details.languages[0]}</li>
+                <li>${details.languages[1]}</li>
+                <li>${details.languages[2]}</li>
+            </ul>
+        </div>
+        <button id="${details.id}" type="button" class="modal-open" data-modal="modal-${details.id}">See Project</button>
+    </div>
+    `
+}
