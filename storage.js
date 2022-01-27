4a1600962bf35ec2869d8bfe29b62a96fe838649
Ahmed Hossam <1318039@gmail.com>
2022-01-27 18:01:39 +0200

    Fix localstorage

--- storage.js
+++ storage.js
@@ -1,33 +1,41 @@
 window.onload = function storeInputs() {
-    let lStorage = {}
-    
-    const nameInput = document.getElementById('name');
-    const nameValue = document.getElementById('name').value;
-    const emailInput = document.getElementById('email');
-    const emailValue = document.getElementById('email').value;
-    const messageInput = document.getElementById('message');
-    const messageValue = document.getElementById('message').value;
+  let lStorage = {};
 
-    nameInput.addEventListener('change', (event) => {
-        lStorage = {...lStorage, "name" : event.target.value}
-        console.log(nameValue)
-        console.log(lStorage)
-    });
+  const nameInput = document.getElementById("name");
+  let nameValue = document.getElementById("name").value;
+  const emailInput = document.getElementById("email");
+  let emailValue = document.getElementById("email").value;
+  const messageInput = document.getElementById("message");
+  let messageValue = document.getElementById("message").value;
 
-    emailInput.addEventListener('change', (event) => {
-        lStorage = {...lStorage, "email" : event.target.value}
-        console.log(lStorage)
-    });
-    
-    messageInput.addEventListener('change', (event) => {
-        lStorage = {...lStorage, "message" : event.target.value}
-        console.log(lStorage)
-    });
+  if (localStorage.getItem("contact-form")) {
+    lStorage = JSON.parse(localStorage.getItem("contact-form"));
+    console.log(lStorage);
 
-    localStorage.setItem(JSON.stringify(lStorage))
-}
+    nameInput.value = lStorage.name;
+    emailInput.value = lStorage.email;
+    messageInput.value = lStorage.message;
+  }
 
+  nameInput.addEventListener("change", (event) => {
+    lStorage = { ...lStorage, name: event.target.value };
+    console.log(nameValue);
+    console.log(lStorage);
+    localStorage.setItem("contact-form", JSON.stringify(lStorage));
+  });
 
+  emailInput.addEventListener("change", (event) => {
+    lStorage = { ...lStorage, email: event.target.value };
+    console.log(lStorage);
+    localStorage.setItem("contact-form", JSON.stringify(lStorage));
+  });
+
+  messageInput.addEventListener("change", (event) => {
+    lStorage = { ...lStorage, message: event.target.value };
+    console.log(lStorage);
+    localStorage.setItem("contact-form", JSON.stringify(lStorage));
+  });
+};
 
 //     document.getElementById('name').innerHTML = localStorage.getItem("name");
 //     document.getElementById('email').innerHTML = localStorage.getItem("email");
